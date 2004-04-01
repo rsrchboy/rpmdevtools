264382bb8c2d5b31d7849e3d499dbb24ad6d9502
scop <scop>
2004-04-01 19:31:08 +0000

    Robustify.

--- mkdist.sh
+++ mkdist.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # Helper script for creating the fedora-rpmdevtools dist tarball.
-# $Id: mkdist.sh,v 1.1 2004/02/08 19:32:45 scop Exp $
+# $Id: mkdist.sh,v 1.2 2004/04/01 19:31:08 scop Exp $
 #
 # Author:  Ville Skyttä <ville.skytta at iki.fi>
 # License: GPL
@@ -10,13 +10,21 @@
 
 set -e
 
+trap cleanup EXIT
+tmpdir=
+
+cleanup()
+{
+    [ -z "$tmpdir" ] || rm -rf "$tmpdir"
+}
+
 if [ -z "$1" ] ; then
     echo "Usage: $0 <version>" >&2
     exit 1
 fi
 
 pwd=`pwd`
-tmpdir=`mktemp -d`
+tmpdir=`mktemp -d /tmp/rpmdevtools.XXXXXX`
 mkdir -p $tmpdir/fedora-rpmdevtools-$1
 cp -pR * $tmpdir/fedora-rpmdevtools-$1
 find $tmpdir -type d -name CVS | xargs -r rm -rf
@@ -27,4 +35,3 @@ cd $tmpdir
 tar --owner=0 --group=0 -jcvf \
   $pwd/fedora-rpmdevtools-$1.tar.bz2 fedora-rpmdevtools-$1
 cd $pwd
-rm -rf $tmpdir
